147de434b463bcd4f877fa85b64a471c40034088
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-07-29 09:45:28 +0900

    test HR() complite

--- Source/Utill/D3DUtill.h
+++ Source/Utill/D3DUtill.h
@@ -13,8 +13,8 @@
 			LPWSTR output; \
 			FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPTSTR)&output, 0, NULL); \
 			std::wostringstream outs;\
-			outs << std::format(L"{} {} {} {}", TEXT(__FILE__), (DWORD)__LINE__, L#x, output);\
-			MessageBox(NULL, output, TEXT("ERROR"), MB_OK);\
+			outs << std::format(L"File: {}\nLine: {}\nFunc: {}\nError Msg: {}", TEXT(__FILE__), (DWORD)__LINE__, L#x, output);\
+			MessageBox(NULL, outs.str().c_str(), TEXT("ERROR"), MB_OK);\
 		}\
 	}
 	#endif
